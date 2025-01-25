d0ee6ce3a6029eefa5b8c923c31c8fa061cf34f9
henriquepablo <pablo269704@gmail.com>
2025-01-25 19:56:40 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by pablo henrique on 25/01/25.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDeliveryTests/ChefDeliveryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryTests.swift
+//  ChefDeliveryTests
+//
+//  Created by pablo henrique on 25/01/25.
+//
+
+import Testing
+@testable import ChefDelivery
+
+struct ChefDeliveryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
