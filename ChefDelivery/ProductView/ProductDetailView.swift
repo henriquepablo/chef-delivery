d51d3737d0ededf495201045d6d56d2a57daa844
henriquepablo <pablo269704@gmail.com>
2025-01-29 20:59:25 -0300

    feat: create screen product detail view

--- ChefDelivery/ProductView/ProductDetailView.swift
+++ ChefDelivery/ProductView/ProductDetailView.swift
@@ -10,8 +10,81 @@ import SwiftUI
 struct ProductDetailView: View {
     let prodcut: ProductType
     
+    @State private var productQunatity: Int = 1
+    
     var body: some View {
-        Text(prodcut.name)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                ContainerRelativeShape()
+                    .shadow(radius: 20)
+                    .frame(width: .infinity, height: 230)
+                
+                Text(prodcut.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(prodcut.description)
+                    .padding(.horizontal)
+                
+                Text(prodcut.formattedPrice)
+                    .font(.title3)
+                    .bold()
+                    .padding(.horizontal)
+            }
+            
+            Spacer()
+            
+            VStack(spacing: 16) {
+                Text("Quantidade")
+                    .font(.title3)
+                    .bold()
+                
+                HStack {
+                    Button {
+                        if (productQunatity > 1) {
+                            productQunatity -= 1
+                        }
+
+                    } label: {
+                        Image(systemName: "minus.circle.fill")
+                            .font(.title)
+                            .bold()
+                    }
+                    Text("\(productQunatity)")
+                        .font(.title2)
+                        .bold()
+                    
+                    Button {
+                        productQunatity += 1
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.title)
+                            .bold()
+                    }
+                }
+            }
+            
+            Spacer()
+            
+            Button  {
+                print("botão pressionado")
+            } label: {
+                HStack {
+                    Image(systemName: "cart")
+                        
+                    Text("Adicionar ao Carrinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical, 16)
+                .font(.title3)
+                .bold()
+                .background(Color("ColorRed"))
+                .foregroundStyle(.white)
+                .cornerRadius(32)
+                .shadow(color: Color("ColorRed").opacity(0.5), radius: 10, x: 6, y: 8)
+            }
+        }
     }
 }
 
